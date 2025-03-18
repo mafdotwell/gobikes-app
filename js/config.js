7ee87dc725247f6244849f8e07f4fbe09498d7bb
mafdotwell <157192382+mafdotwell@users.noreply.github.com>
2025-03-18 14:34:13 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_uR668SHMT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'q1qeboema59pvmqq1ofcmuvg6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-2' // e.g. us-east-2
+        userPoolId: 'eu-north-1_7xUepZdds', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'mtrs2ajollbbnpthi9lhi1g33', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
